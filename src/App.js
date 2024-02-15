2269c217a3661fe2c5115580e3a3f6c44351a4a7
erik <eriktrujillo0001@gmail.com>
2024-02-15 09:28:36 -0600

    completo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import { Provider } from 'react-redux';
+import store from "./redux/store";
+import Header from './screens/header/header';
+import Footer from './screens/footer/footer';
+import Home from './screens/home/home';
+import PopularSeries from './screens/popularSeries/series';
+import PopularMovies from './screens/popularMovies/movies';
 
 function App() {
+
+  const [currentScreen, setCurrentScreen] = useState('home');
+
+  const handleHomeClick = () => {
+    setCurrentScreen('home');
+  }
+
+  const handleSeriesClick = () => {
+    setCurrentScreen('series');
+  };
+
+  const handleMoviesClick = () => {
+    setCurrentScreen('movies');
+  }
+
+
   return (
+    <Provider store={store}>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header handleHomeClick={handleHomeClick}/>
+      
+      {currentScreen === 'home' && <Home handleSeriesClick={handleSeriesClick} handleMoviesClick={handleMoviesClick}/>}
+      {currentScreen === 'series' && <PopularSeries/>}
+      {currentScreen === 'movies' && <PopularMovies/>} 
+      
+
+      <Footer/>
     </div>
+    </Provider>
   );
 }
 
